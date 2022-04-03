1326bdc5c77399ce9f872f85bdd48712e1ba481f
er0k408 <ericolson44@gmail.com>
2022-04-03 16:13:49 -0700

    First Commit

---
+++ Linux/dealersworking.sh
@@ -0,0 +1,13 @@
+cat 0310_Dealer_schedule | grep "05:00:00 AM" | awk '{print $5,$6}' >> Dealers_working_during_losses
+cat 0310_Dealer_schedule | grep "08:00:00 AM" | awk '{print $5,$6}' >> Dealers_working_during_losses
+cat 0310_Dealer_schedule | grep "02:00:00 PM" | awk '{print $5,$6}' >> Dealers_working_during_losses
+cat 0310_Dealer_schedule | grep "08:00:00 PM" | awk '{print $5,$6}' >> Dealers_working_during_losses
+cat 0310_Dealer_schedule | grep "11:00:00 PM" | awk '{print $5,$6}' >> Dealers_working_during_losses
+cat 0312_Dealer_schedule | grep "05:00:00 AM" | awk '{print $5,$6}' >> Dealers_working_during_losses
+cat 0312_Dealer_schedule | grep "08:00:00 AM" | awk '{print $5,$6}' >> Dealers_working_during_losses
+cat 0312_Dealer_schedule | grep "02:00:00 PM" | awk '{print $5,$6}' >> Dealers_working_during_losses
+cat 0312_Dealer_schedule | grep "08:00:00 PM" | awk '{print $5,$6}' >> Dealers_working_during_losses
+cat 0312_Dealer_schedule | grep "11:00:00 PM" | awk '{print $5,$6}' >> Dealers_working_during_losses
+cat 0315_Dealer_schedule | grep "05:00:00 AM" | awk '{print $5,$6}' >> Dealers_working_during_losses
+cat 0315_Dealer_schedule | grep "08:00:00 AM" | awk '{print $5,$6}' >> Dealers_working_during_losses
+cat 0315_Dealer_schedule | grep "02:00:00 PM" | awk '{print $5,$6}' >> Dealers_working_during_losses
